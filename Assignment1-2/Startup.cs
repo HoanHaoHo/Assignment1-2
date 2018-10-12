aa3be02cd19f332d34188ef3f6cbb0674100e384
Hao Ho <37636748+haomap123456@users.noreply.github.com>
2018-10-12 10:10:30 -0500

    Add project files.

---
+++ Assignment1-2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment1_2.Startup))]
+namespace Assignment1_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
